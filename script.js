8ae5afee8cef258fe004f675e45ced636a1665b1
suvakantamalik <134865642+suvakantamalik@users.noreply.github.com>
2024-08-22 10:59:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
+
+js code:
+
 const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+  let prc = document.getElementsByClassName("price");
+	let sum = 0;
+	for(let ele of prc){
+		sum += parseFloat(ele.textContent);
+	}
+	console.log(sum);
+	let tbl = document.getElementById("table");
+	let row = tbl.insertRow();
+	let col1 = row.insertCell();
+	col1.textContent("Total: ");
+	let col2 = row.insertCell();
+	col1.textContent("Total: ");
 };
 
 getSumBtn.addEventListener("click", getSum);
